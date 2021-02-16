ea2f74191656a2053d7c37263d57dcd05e8af475
PERREL HYACINTHE <hyacinthe.perrel@etu.unistra.fr>
2021-02-16 10:31:28 +0100

    début d'affichage sdl2 tuto shooter

---
+++ README.md
@@ -0,0 +1,3 @@
+Ce Projet a été commencé en février. On postera ici l'avancé du projet en temps réel ainsi que les problèmes ou réussites rencontrées.
+
+Equipe Thomas et Hyacinthe
---
+++ Tetris.cpp
@@ -0,0 +1,17 @@
+/*************************************************************************/
+/*                                                                       */
+/*                           TETRIS IN A NUTCHELL                        */
+/*                                                                       */
+/*************************************************************************/
+
+/* Headers */
+
+#include <graphics.h>
+#include <conio.h>
+#include <stdio.h>
+#include <stdlib.h>
+#include <dos.h>
+
+/* a premiere vue, je dirai qu'il faut une classe pour le plateau de jeu,
+une classe pour les blocs, et plein de fonction pour rendre tout ça dynamique*/
+
